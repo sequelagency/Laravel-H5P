0250cf70aab0fe65d814d5ad5da26b96f97505ad
s.dauga <s.dauga@exatech-group.com>
2019-11-04 10:20:18 +0100

    protect

--- src/LaravelH5p/LaravelH5p.php
+++ src/LaravelH5p/LaravelH5p.php
@@ -223,7 +223,7 @@ class LaravelH5p
     {
         $settings = [
             'baseUrl'            => config('laravel-h5p.domain'),
-            'url'                => config('laravel-h5p.domain').'/api/h5p_protect?'.(Auth::check() ? 'token='.auth()->tokenById(Auth::id()).'&' : '').'data=',//self::get_h5p_storage(), // for uploaded
+            'url'                => config('laravel-h5p.domain').'/api/h5p_protect?'.(Auth::check() ? 'token='.auth()->claims(['referer' => app('Illuminate\Routing\UrlGenerator')->previous()])->tokenById(Auth::id()).'&' : '').'data=',//self::get_h5p_storage(), // for uploaded
             'postUserStatistics' => (config('laravel-h5p.h5p_track_user', true) === '1') && Auth::check(),
             'ajax'               => [
                 'setFinished'     => route('h5p.ajax.finish'),
